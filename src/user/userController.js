e525338e15eec2948b5a4730a5dd3118f475878b
Lovish Duggal <lovishduggal11@gmail.com>
2024-07-11 15:57:12 +0530

    add user search functionality

--- src/user/userController.js
+++ src/user/userController.js
@@ -36,8 +36,38 @@ const createUser = async (req, res, next) => {
 };
 
 const getAllUsers = async (req, res, next) => {
+    const { q: searchTerm } = req.query;
+
     try {
-        const users = await userModal.find();
+        // Check if the search term is valid
+        if (searchTerm && !/^[a-zA-Z0-9]+$/.test(searchTerm)) {
+            const error = createHttpError(400, 'Invalid query');
+            return next(error);
+        }
+
+        // Build the query based on the search term
+        const query = searchTerm
+            ? {
+                  $or: [
+                      {
+                          $expr: {
+                              $regexMatch: {
+                                  input: {
+                                      $concat: ['$firstName', ' ', '$lastName'],
+                                  },
+                                  regex: searchTerm,
+                                  options: 'i',
+                              },
+                          },
+                      },
+                      { email: { $regex: searchTerm, $options: 'i' } },
+                  ],
+              }
+            : {};
+
+        // Find users based on the query and sort them by _id in descending order
+        const users = await userModal.find(query).sort({ _id: -1 });
+
         return res.status(200).json({ data: users });
     } catch (error) {
         return next(error);
